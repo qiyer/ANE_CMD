dd090b10ef0e4fbe7d334d359fed65ebd42b7ebf
qiyer <justqiye@163.com>
2014-09-26 12:04:39 +0800

    Revert "1.0.0"
    This reverts commit 3ba9d6b3f747737f9d23bf26af910963826bd805.

--- cmd/swf.bat
+++
@@ -1,3 +0,0 @@
-C:\AIRSDK\bin\amxmlc +configname=airmobile -external-library-path+=ane/AirInAppPurchase.ane,ane/com.xiawu.update.ane,ane/AirFacebook.ane,ane/appsflyer.ane,ane/metaps_sdk_air_extension_sdk_v3_0_0.ane,ane/TapjoyExtension.ane -library-path+=..\..\..\..\XiaWu\XiaWuMain\bin\XiaWuMain.swc,..\..\..\..\XiaWu\ZtUtil\bin\ZtUtil.swc,metaps_sdk_air_extension_sdk_v3_0_0.swc,TapjoyExtension.swc -output=main.swf ..\..\..\..\XiaWu\XiaWuAndroid\src\XiaWuGoogle.as
-pause
-
